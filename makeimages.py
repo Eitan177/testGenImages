6f6b0d3284aaf7539a2084d205594f9f8f77cf09
Eitan Halper-Stromberg <Eitan177@gmail.com>
2021-10-14 15:45:06 -0400

    Update makeimages.py

--- makeimages.py
+++ makeimages.py
@@ -27,7 +27,7 @@ if file_exists != True:
     st.spinner('Downloading our pkl file model')
     gdown.download(url,"network-snapshot-025000.pkl")
 labelmake=st.selectbox('what type of images do you want to generate',tcga_w_annot['type'].tolist())
-nummake=tcga_w_annot['num_type'].iloc[tcga_w_annot['type']==labelmake]
+nummake=tcga_w_annot['type_num'].iloc[tcga_w_annot['type']==labelmake]
 num=st.select_slider('images to show',[1,2,3,4,5,6,7,8])
 seeds =[int(ii) for ii in np.absolute(np.random.randn(num))*100]
 generate_images(easing='linear',interpolation='linear',increment=.01,network_pkl='network-snapshot-025000.pkl',process='image',random_seed=0,diameter=100.0,scale_type='pad',seeds=seeds,space='z',truncation_psi=1,noise_mode='const',outdir='.',class_idx=make,size=False,frames=240,fps=24,start=0.0,stop=1.0,projected_w=None)
