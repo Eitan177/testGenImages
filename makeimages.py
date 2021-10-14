b6ce5619d837a7f778ccfc21e62f27aa88214db7
Eitan Halper-Stromberg <Eitan177@gmail.com>
2021-10-14 13:40:38 -0400

    Update makeimages.py

--- makeimages.py
+++ makeimages.py
@@ -25,7 +25,7 @@ if file_exists != True:
     gdown.download(url,"network-snapshot-025000.pkl")
 make=st.selectbox('what type of images do you want to generate',np.arange(0,32).tolist())
 num=st.select_slider('images to show',[1,2,3,4,5,6,7,8])
-seeds =list(np.absolute(np.random.randn(num))*100)
+seeds =[int(ii) for ii in np.absolute(np.random.randn(num))*100]
 generate_images(easing='linear',interpolation='linear',increment=.01,network_pkl='network-snapshot-025000.pkl',process='image',random_seed=0,diameter=100.0,scale_type='pad',seeds=seeds,space='z',truncation_psi=1,noise_mode='const',outdir='.',class_idx=make,size=False,frames=240,fps=24,start=0.0,stop=1.0,projected_w=None)
         ##os.system("python generate.py --outdir=. --seeds="+str(0)+"-"+str(9)+" --class="+str(ii)+' --network=network-snapshot-025000.pkl')
 
